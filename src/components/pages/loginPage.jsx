0c6351d36b7e0f50bd6775ecd22d4ac5d213b209
Dina <dina.olufsen@hotmail.com>
2023-04-21 17:19:00 +0200

    working on login page

--- src/components/pages/loginPage.jsx
+++ src/components/pages/loginPage.jsx
@@ -1,11 +1,70 @@
-import { useEffect } from "react";
+// import { Button } from "../styles/buttons.styles";
+import React, { useEffect } from "react";
+// import { useForm } from "react-hook-form";
+// import { yupResolver } from '@hookform/resolvers/yup';
+// import * as yup from 'yup';
+
+// const schema = yup
+//   .object({
+//     fullName: yup
+//     .string()
+//     .min(3, 'Must contain more than 3 characters')
+//     .required('Please fill in this field')
+//     .typeError('Please write your full name here'),
+//     subject: yup
+//     .string()
+//     .min(3, 'Must contain more than 3 characters')
+//     .required('Please fill in this field')
+//     .typeError('Please write your subject here'),
+//     email: yup
+//       .string()
+//       .email('Please enter a valid email address')
+//       .required('Please enter a valid email address')
+//       .typeError('Please enter a valid email address'),
+//     body: yup
+//       .string()
+//       .min(3, 'Must contain more than 3 characters')
+//       .required('Please fill in this field')
+//       .typeError('Please write your message here'),
+//   })
+//   .required();
 
 function LoginPage() {
   useEffect(() => {
     document.title = "Holidaze | Log in"
  }, []);
  
-    return <div>Log in</div>;
+    return (
+    <main className="container d-flex justify-content-center p-5">
+      <div className="col-8 row">
+        <h1 className='contact-heading'>
+          Log in
+        </h1>
+        {/* <form onSubmit={handleSubmit(onSubmitHandler)}>
+          <div>
+            <label htmlFor='fullName'>Full Name</label>
+            <Input {...register("fullName")} />
+            <Error>{errors.fullName?.message}</Error>
+          </div>
+          <div>
+            <label htmlFor='subject'>Subject</label>
+            <Input {...register("subject")} />
+            <Error>{errors.subject?.message}</Error>
+          </div>
+          <div>
+            <label htmlFor='email'>E-mail</label>
+            <Input {...register("email")} />
+            <Error>{errors.email?.message}</Error>
+          </div>
+          <div>
+            <label htmlFor='body'>Message</label>
+            <TextArea {...register("body")} />
+            <Error>{errors.body?.message}</Error>
+          </div>
+          <Button className="contact-button" type="submit">Send</Button>
+        </form> */}
+      </div>
+    </main>);
   }
 
   export default LoginPage;
