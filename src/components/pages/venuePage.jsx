aff01fed922245c786c629a2c28d7c3653a479ed
Dina <dina.olufsen@hotmail.com>
2023-04-24 17:22:37 +0200

    updated venue page to fetch params

--- src/components/pages/venuePage.jsx
+++ src/components/pages/venuePage.jsx
@@ -1,8 +1,11 @@
+// import { useParams } from 'react-router-dom';
 import { useEffect } from "react";
 
 function VenuePage() {
+  // let params = useParams();
+
   useEffect(() => {
-    document.title = "Holidaze | Venue"
+    document.title = "Holidaze | Venue"; //make it show title after fetched from API
  }, []);
 
     return <div>Venue</div>;
