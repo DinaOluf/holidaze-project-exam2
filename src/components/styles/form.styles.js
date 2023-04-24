d9ffeda0861b8f769e909a6805c7f11a73f02a22
Dina <dina.olufsen@hotmail.com>
2023-04-24 15:17:07 +0200

    form styling

---
+++ src/components/styles/form.styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+    border: solid 1px #495B70;
+    height: 2.5rem;
+    border-radius: 8px;
+    font-size: 1.2em;
+
+    :focus {
+        outline: solid 2px #495B70;
+    }
+`;
+
+const Error = styled.div`
+    color: #495B70;
+    min-height: 1.2rem;
+`;
+
+export { Input, Error };
