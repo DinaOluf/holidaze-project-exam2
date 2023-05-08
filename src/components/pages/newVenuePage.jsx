4934eb14020b2735b290e79c7f13a1238641202b
Dina <dina.olufsen@hotmail.com>
2023-05-08 15:57:13 +0200

    Creating create venue page

--- src/components/pages/newVenuePage.jsx
+++ src/components/pages/newVenuePage.jsx
@@ -1,3 +1,4 @@
+import { Button } from "../styles/buttons.styles";
 import { useEffect } from "react";
 
 function NewVenuePage() {
@@ -5,7 +6,31 @@ function NewVenuePage() {
     document.title = "Holidaze | New Venue"
  }, []);
 
-    return <div>Create new venue</div>;
+    return <main id="container p-5">
+        <div className="d-flex justify-content-center mt-4">
+          <div className='col-11 col-sm-9 col-xl-7 rounded-5 bg-light overflow-hidden p-4'>
+            <h1>Create a new venue</h1>
+            <form>
+                <div className="row flex-wrap">
+                  <div className="col">
+                    <div>Title</div>
+                    <div>Guests + Price</div>
+                  </div>
+                  <div className="col">
+                    <div>Direct Img Url</div>
+                    <div>Select</div>
+                  </div>
+                </div>
+                <div>
+                  Textarea
+                </div>
+                <div className="d-flex justify-content-center">
+                  <Button>Post Venue</Button>
+                </div>
+            </form>
+          </div>
+        </div>
+    </main>;
   }
 
   export default NewVenuePage;
