bfa0cba8f54debf03fff785ebd9396b5374a713b
Dina <dina.olufsen@hotmail.com>
2023-05-11 13:36:46 +0200

    Added Venue Manager's Venues on profile

--- src/components/pages/profilePage.jsx
+++ src/components/pages/profilePage.jsx
@@ -107,7 +107,7 @@ const { register, handleSubmit, formState: { errors }, reset } = useForm({
       buttons: [
         {
           label: 'Yes',
-          onClick: () => onSubmitHandler(e)
+          onClick: () => onDeleteHandler(e)
         },
         {
           label: 'No',
@@ -116,7 +116,7 @@ const { register, handleSubmit, formState: { errors }, reset } = useForm({
     });
   }
 
-  const onSubmitHandler = async (id) => {
+  const onDeleteHandler = async (id) => {
     const url = "https://api.noroff.dev/api/v1/holidaze/bookings/"+id;
     const token = localStorage.getItem("Token");
   
@@ -157,7 +157,7 @@ const { register, handleSubmit, formState: { errors }, reset } = useForm({
 
 
   return <main id="container p-5">
-    <div className="d-flex justify-content-center mt-5">
+    <div className="d-flex flex-column align-items-center mt-5">
       <ContainerCard className='col-11 col-sm-9 col-xl-7 rounded-5'>
         <div className="d-flex align-items-center">
           <div className=" position-relative">
@@ -202,105 +202,110 @@ const { register, handleSubmit, formState: { errors }, reset } = useForm({
             <div>{data.email}</div>
           </div>
         </div>
-        <hr />
-        <h2 className="mt-5">Upcoming Bookings</h2>
-        {data.bookings
-        ? data.bookings.map((data) => (
-          <div key={data.venue.id}>
-            { data.dateTo >= new Date().toISOString()
-                  ? <div className="d-flex flex-wrap mb-5">
-                  <VenueCard className="position-relative" title="Takes you to the venue page" to={`/venue/${data.venue.id}`}>
-                  <div className="card-img-wrap">
-                      { data.venue.media.length === 0 
-                          ?  <img src={PlaceholderImg} className="venue-images" alt="Venue" />
-                          :  <img src={data.venue.media[0]} className="venue-images" alt="Venue" onError={(e)=>{ if (e.target.src !== PlaceholderImg) 
-                          { e.target.onerror = null; e.target.src=PlaceholderImg; } }} /> 
-                      }
-                  </div>
-                  <div className="p-2 h-50">
-                      { data.venue.name.length >= 20 
-                          ? <h2 className="m-0 fs-4">{cut20(data.venue.name)}</h2>
-                          : <h2 className="m-0 fs-4">{data.venue.name}</h2>
-                      }
-                      <div className="d-flex justify-content-between mb-3">
-                          <div>{data.venue.price},-</div>
-                          <div className="d-flex align-items-center">
-                              <div>{data.venue.maxGuests}</div>
-                              <img src={PersonIcon} className="person-icon mt-1" alt="person icon" />
+        {data.bookings && data.bookings.length !== 0
+        ? <>
+          <hr />
+          <h2 className="mt-5">Upcoming Bookings</h2>
+          {data.bookings.map((data) => (
+            <div key={data.venue.id}>
+              { data.dateTo >= new Date().toISOString()
+                    ? <div className="d-flex flex-wrap mb-5">
+                    <VenueCard className="position-relative" title="Takes you to the venue page" to={`/venue/${data.venue.id}`}>
+                    <div className="card-img-wrap">
+                        { data.venue.media.length === 0 
+                            ?  <img src={PlaceholderImg} className="venue-images" alt="Venue" />
+                            :  <img src={data.venue.media[0]} className="venue-images" alt="Venue" onError={(e)=>{ if (e.target.src !== PlaceholderImg) 
+                            { e.target.onerror = null; e.target.src=PlaceholderImg; } }} /> 
+                        }
+                    </div>
+                    <div className="p-2 h-50">
+                        { data.venue.name.length >= 20 
+                            ? <h2 className="m-0 fs-4">{cut20(data.venue.name)}</h2>
+                            : <h2 className="m-0 fs-4">{data.venue.name}</h2>
+                        }
+                        <div className="d-flex justify-content-between mb-3">
+                            <div>{data.venue.price},-</div>
+                            <div className="d-flex align-items-center">
+                                <div>{data.venue.maxGuests}</div>
+                                <img src={PersonIcon} className="person-icon mt-1" alt="person icon" />
+                            </div>
+                        </div>
+                        <div className="d-flex justify-content-end my-1 gap-1">
+                            { data.venue.meta.wifi 
+                                ? <img src={WifiIcon} className="card-icons" alt="wifi icon"/> 
+                                : "" }
+                            { data.venue.meta.parking 
+                                ? <img src={ParkingIcon} className="card-icons" alt="parking icon"/> 
+                                : "" }
+                            { data.venue.meta.breakfast 
+                                ? <img src={FoodIcon} className="card-icons" alt="breakfast icon"/> 
+                                : "" }
+                            { data.venue.meta.parking 
+                                ? <img src={PetsIcon} className="card-icons" alt="pets icon"/>
+                                : "" }
+                        </div>
+                    </div>
+                </VenueCard>
+                { data.dateFrom >= new Date().toISOString()
+                  ? <>
+                      <div className='d-flex flex-column justify-content-between flex-wrap ms-2'>
+                        <div className='d-flex flex-column fs-5'>
+                          <label htmlFor={`dateArrival-${data.id}`}>Date of arrival</label>
+                          <div id={`dateArrival-${data.id}`}>{data.dateFrom.slice(0, 10)}</div>
+                        </div>
+                        <div className='d-flex flex-column fs-5'>
+                          <label htmlFor={`dateDeparture-${data.id}`}>Date of departure</label>
+                          <div id={`dateDeparture-${data.id}`}>{data.dateTo.slice(0, 10)}</div>
+                        </div>
+                        <div className='d-flex flex-column fs-5'>
+                          <label htmlFor={`numberGuests-${data.id}`}>Guest(s)</label>
+                          <div className='d-flex'>
+                            <div id={`numberGuests-${data.id}`} className='text-end'>{data.guests}</div>
+                            <PersonIconStyle src={PersonIcon} alt='Person icon' />
                           </div>
-                      </div>
-                      <div className="d-flex justify-content-end my-1 gap-1">
-                          { data.venue.meta.wifi 
-                              ? <img src={WifiIcon} className="card-icons" alt="wifi icon"/> 
-                              : "" }
-                          { data.venue.meta.parking 
-                              ? <img src={ParkingIcon} className="card-icons" alt="parking icon"/> 
-                              : "" }
-                          { data.venue.meta.breakfast 
-                              ? <img src={FoodIcon} className="card-icons" alt="breakfast icon"/> 
-                              : "" }
-                          { data.venue.meta.parking 
-                              ? <img src={PetsIcon} className="card-icons" alt="pets icon"/>
-                              : "" }
-                      </div>
-                  </div>
-              </VenueCard>
-              { data.dateFrom >= new Date().toISOString()
-                ? <>
-                    <div className='d-flex flex-column justify-content-between flex-wrap ms-2'>
-                      <div className='d-flex flex-column fs-5'>
-                        <label htmlFor={`dateArrival-${data.id}`}>Date of arrival</label>
-                        <div id={`dateArrival-${data.id}`}>{data.dateFrom.slice(0, 10)}</div>
-                      </div>
-                      <div className='d-flex flex-column fs-5'>
-                        <label htmlFor={`dateDeparture-${data.id}`}>Date of departure</label>
-                        <div id={`dateDeparture-${data.id}`}>{data.dateTo.slice(0, 10)}</div>
-                      </div>
-                      <div className='d-flex flex-column fs-5'>
-                        <label htmlFor={`numberGuests-${data.id}`}>Guest(s)</label>
-                        <div className='d-flex'>
-                          <div id={`numberGuests-${data.id}`} className='text-end'>{data.guests}</div>
-                          <PersonIconStyle src={PersonIcon} alt='Person icon' />
+                        </div>
+                        <div className="d-flex flex-wrap gap-2">
+                          <ButtonSmaller2 onClick={() => onClickConfirm(data.id)}>Cancel</ButtonSmaller2>
+                          <ButtonSmaller title="Edit this booking" to={`/booking/${data.id}`}>Edit</ButtonSmaller>
                         </div>
                       </div>
-                      <div className="d-flex flex-wrap gap-2">
-                        <ButtonSmaller2 onClick={() => onClickConfirm(data.id)}>Cancel</ButtonSmaller2>
-                        <ButtonSmaller title="Edit this booking" to={`/booking/${data.id}`}>Edit</ButtonSmaller>
-                      </div>
-                    </div>
-                  </>
-                : <>
-                    <div className='d-flex flex-column justify-content-between flex-wrap ms-2'>
-                      <div className='d-flex flex-column fs-5'>
-                        <label htmlFor={`dateArrival-${data.id}`}>Date of arrival</label>
-                        <div id={`dateArrival-${data.id}`}>{data.dateFrom.slice(0, 10)}</div>
-                      </div>
-                      <div className='d-flex flex-column fs-5'>
-                        <label htmlFor={`dateDeparture-${data.id}`}>Date of departure</label>
-                        <div id={`dateDeparture-${data.id}`}>{data.dateTo.slice(0, 10)}</div>
-                      </div>
-                      <div className='d-flex flex-column fs-5'>
-                        <label htmlFor={`numberGuests-${data.id}`}>Guest(s)</label>
-                        <div className='d-flex'>
-                          <div id={`numberGuests-${data.id}`} className='text-end'>{data.guests}</div>
-                          <PersonIconStyle src={PersonIcon} alt='Person icon' />
+                    </>
+                  : <>
+                      <div className='d-flex flex-column justify-content-between flex-wrap ms-2'>
+                        <div className='d-flex flex-column fs-5'>
+                          <label htmlFor={`dateArrival-${data.id}`}>Date of arrival</label>
+                          <div id={`dateArrival-${data.id}`}>{data.dateFrom.slice(0, 10)}</div>
+                        </div>
+                        <div className='d-flex flex-column fs-5'>
+                          <label htmlFor={`dateDeparture-${data.id}`}>Date of departure</label>
+                          <div id={`dateDeparture-${data.id}`}>{data.dateTo.slice(0, 10)}</div>
+                        </div>
+                        <div className='d-flex flex-column fs-5'>
+                          <label htmlFor={`numberGuests-${data.id}`}>Guest(s)</label>
+                          <div className='d-flex'>
+                            <div id={`numberGuests-${data.id}`} className='text-end'>{data.guests}</div>
+                            <PersonIconStyle src={PersonIcon} alt='Person icon' />
+                          </div>
+                        </div>
+                        <div className="d-flex mb-3 flex-wrap gap-2">
+                          <Error title="If your booking is in less than 24 hours you cannot change your booking.">Unable to edit due to 24hr restriction.</Error>
                         </div>
                       </div>
-                      <div className="d-flex mb-3 flex-wrap gap-2">
-                        <Error title="If your booking is in less than 24 hours you cannot change your booking.">Unable to edit due to 24hr restriction.</Error>
-                      </div>
-                    </div>
-                  </>
-              }
+                    </>
+                }
+              </div>
+              : ""
+            }
             </div>
-            : ""
-          }
-          </div>
-        ))
-        : ""}
+          ))}
+        </>
+        : ""
+        }
+
+        {data.bookings && data.bookings.length !== 0
+        ? <>
         <h2 className="mt-5">Old Bookings</h2>
-        {data.bookings
-        ? data.bookings.map((data) => (
+        {data.bookings.map((data) => (
           <div key={data.venue.id}>
             { data.dateTo < new Date().toISOString()
                   ? <div className="d-flex flex-wrap mb-5">
@@ -364,9 +369,56 @@ const { register, handleSubmit, formState: { errors }, reset } = useForm({
             : ""
           }
           </div>
-        ))
+        ))}
+        </>
         : ""}
       </ContainerCard>
+      {data.venueManager && data.venues.length !== 0
+      ? <div className='col-11 col-sm-9 col-xl-7 mt-4'>
+          <h2>{data.name}'s Venues</h2>
+          <div className="d-flex justify-content-evenly gap-4 mt-4 flex-wrap">
+                {data.venues.map((data) => (
+                <VenueCard className="position-relative" key={data.id} to={`/venue/${data.id}`}>
+                    <div className="card-img-wrap">
+                        { data.media.length === 0 
+                            ?  <img src={PlaceholderImg} className="venue-images" alt="Venue" />
+                            :  <img src={data.media[0]} className="venue-images" alt="Venue" onError={(e)=>{ if (e.target.src !== PlaceholderImg) 
+                            { e.target.onerror = null; e.target.src=PlaceholderImg; } }} /> 
+                        }
+                    </div>
+                    <div className="p-2 h-50">
+                        { data.name.length >= 20 
+                            ? <h2 className="m-0 fs-4">{cut20(data.name)}</h2>
+                            : <h2 className="m-0 fs-4">{data.name}</h2>
+                        }
+                        <div className="d-flex justify-content-between mb-3">
+                            <div>{data.price},-</div>
+                            <div className="d-flex align-items-center">
+                                <div>{data.maxGuests}</div>
+                                <img src={PersonIcon} className="person-icon mt-1" alt="person icon" />
+                            </div>
+                        </div>
+                        <div className="d-flex justify-content-end my-1 gap-1">
+                            { data.meta.wifi 
+                                ? <img src={WifiIcon} className="card-icons" alt="wifi icon"/> 
+                                : "" }
+                            { data.meta.parking 
+                                ? <img src={ParkingIcon} className="card-icons" alt="parking icon"/> 
+                                : "" }
+                            { data.meta.breakfast 
+                                ? <img src={FoodIcon} className="card-icons" alt="breakfast icon"/> 
+                                : "" }
+                            { data.meta.parking 
+                                ? <img src={PetsIcon} className="card-icons" alt="pets icon"/>
+                                : "" }
+                        </div>
+                    </div>
+                </VenueCard>
+                ))}
+            </div>
+        </div>
+      : ""
+      }
     </div>
 </main>;
 }
