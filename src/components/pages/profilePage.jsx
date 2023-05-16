3ee5a05ec11a0cde3e9fc3cd85cd3924a70656e0
Dina <dina.olufsen@hotmail.com>
2023-05-16 16:07:54 +0200

    Update profilePage.jsx

--- src/components/pages/profilePage.jsx
+++ src/components/pages/profilePage.jsx
@@ -130,7 +130,7 @@ const { register, handleSubmit, formState: { errors }, reset } = useForm({
   
     try {
       await fetch(url, options);
-      window.location.reload(false);
+      window.location.reload(true);
   
     } catch (error) {
       console.log(error);
