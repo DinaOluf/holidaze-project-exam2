f2ba8a40063bd62487728826b048ca5f1f9d1ff0
Dina <dina.olufsen@hotmail.com>
2023-05-19 19:53:45 +0200

    Made Not Found Page more appealing

--- src/components/pages/notFoundPage.jsx
+++ src/components/pages/notFoundPage.jsx
@@ -1,11 +1,15 @@
 import { useEffect } from "react";
+import { Links } from "../styles/links.style";
 
 function RouteNotFound() {
   useEffect(() => {
     document.title = "Holidaze | Page not found"
  }, []);
 
-    return <div>Page not found</div>;
+    return <main className="container d-flex flex-column justify-content-center align-items-center h-100">
+      <div className="fs-4">Page not found</div>
+      <Links className="fs-5" to='/'>Back to front page</Links>
+  </main>;
   }
 
   export default RouteNotFound;
